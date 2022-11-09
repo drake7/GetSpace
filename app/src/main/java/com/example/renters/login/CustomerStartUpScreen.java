41febe39a1cc8b4345bed2daa85dce6bebf5dbb9
drake7 <deepak.kumar7v@gmail.com>
2022-11-09 12:57:25 -0800

    Initial Commit for the Login Screen

---
+++ app/src/main/java/com/example/renters/login/CustomerStartUpScreen.java
@@ -0,0 +1,16 @@
+package com.example.renters.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.renters.R;
+
+public class CustomerStartUpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_start_up_screen);
+    }
+}
